4e2811f3680f2859da4a92072dcbd94a354081a6
Anton-Osip <63441692+Anton-Osip@users.noreply.github.com>
2021-10-25 11:45:02 +0300

    Slider workbench with banner

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-$(function(){
-
-});
+$(function () {
+	$('.banner-section__slider').slick({
+		dots: true,
+		prevArrow:
+			"<button class='banner-section__slider-btn banner-section__slider-btn-prev'><img src='./images/arow-left.svg' alt=''></button>",
+		nextArrow:
+			"<button class='banner-section__slider-btn banner-section__slider-btn-next'><img src='./images/arow-right.svg' alt=''></button>",
+	})
+})
