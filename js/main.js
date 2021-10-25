4dc04296802f842411a80dad587c4259b988ef08
Anton-Osip <63441692+Anton-Osip@users.noreply.github.com>
2021-10-25 16:19:22 +0300

    Layout of the Categories block

--- js/main.js
+++ js/main.js
@@ -9,7 +9,6 @@ $(function () {
 
 	$('.search__tabs-item').on('click', function (e) {
 		e.preventDefault()
-
 		$('.search__tabs-item').removeClass('search__tabs-item--active')
 		$('.search__content-item').removeClass('search__content-item--active')
 		$(this).addClass('search__tabs-item--active')
